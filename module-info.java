7530c374d49ac5e898bcc580f90c1abce0b63fe6
4hebul3 <123885270+4hebul3@users.noreply.github.com>
2023-03-08 08:50:17 +0300

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,11 @@
+module TETRIX2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires javafx.swt;
+
+    opens application;
+}
